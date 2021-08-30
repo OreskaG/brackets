f3f54622abf23bc7d3c8f747f85c067a46c102a6
OreskaG <Ctepanrfo@gmail.com>
2021-08-30 13:41:25 +0300

    feat: add js code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let array = [];
+    for (let bracket of bracketsConfig) {  
+        array.push(bracket.join(''))
+        };
+    for (let i = 0; i < array.length; i++) {
+        if (str.includes(array[i])) {
+            str = str.replace(array[i], '');
+            i =- 1;
+            }
+        };
+ return str.length === 0;
 }
